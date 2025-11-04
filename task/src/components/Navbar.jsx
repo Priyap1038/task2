cc0a01dc4f2c0ac1fdabce33258eaa32a24865fd
Priya_P <124154881+Priyap1038@users.noreply.github.com>
2025-11-04 23:08:26 +0530

    updated

--- task/src/components/Navbar.jsx
+++ task/src/components/Navbar.jsx
@@ -61,6 +61,7 @@ const Navbar = () => {
       </div>
 
       <div className="info">
+        
         <div className="info2">
         <img
           src="https://cdn-icons-png.flaticon.com/128/3917/3917705.png"
